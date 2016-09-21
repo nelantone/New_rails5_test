c53bab688d4b974dcd79adef0ee1a19da0cae42a
Toño Serna <antonelo@gmail.com>
2016-09-21 17:12:02 +0200

    6.15 Add Payment model & move controller logic inside

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,21 +1,13 @@
 class PaymentsController < ApplicationController
   def create
-    @product = Product.find(params[:product_id])
-    @user = current_user
+    product = Product.find(params[:product_id])
+    # @user = current_user
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
-      charge = Stripe::Charge.create(
-        :amount => (@product.price * 100).to_i, # amount in cents, again
-        :currency => "eur",
-        :source => token,
-        :description => params[:stripeEmail]
-      )
-    if charge.paid
-      Order.create(
-        :product_id => @product_id,
-        :user_id => @user_id,
-        :total => @product.price)
+
+    if Payment.charge(product, token, params).paid
+      Payment.create_order(product)
     end
 
     flash[:success] = "Payment processed successfully"
@@ -27,6 +19,6 @@ class PaymentsController < ApplicationController
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
 
-    UserMailer.create(@user, charge ).deliver_now
+    # UserMailer.create(@user, charge ).deliver_now
   end
 end
